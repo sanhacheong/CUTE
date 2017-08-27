d40c0dac3c7ca9411960f6dbdec41b22aa3e3680
sanhacheong <sanha.cheong@gmail.com>
2017-08-26 19:43:45 -0700

    .gitignore ignores all .dat files (too large for GitHub)

---
+++ CUTE/ur_test/ProcessCUTEFormat.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+
+input_file_name = "./data/galaxies_DR9_CMASS_North_ascii.dat"
+
+ra, dec, z, weight_fkp, weight_noz, weight_cp, weight_sdc = np.loadtxt(input_file_name, delimiter='\t', unpack=True, skiprows=1)
+weight = weight_fkp * weight_sdc * (weight_noz + weight_cp - 1)
+
+output_file_name = ".".join(input_file_name.split('.')[:-1]) + "_CUTE.dat"
+
+np.savetxt(output_file_name, np.column_stack([ra, dec, z, weight]), delimiter='\t', fmt='%.8f')
